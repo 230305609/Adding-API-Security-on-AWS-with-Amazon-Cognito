30f199ce571378f16a64c976d0bb88125b1d8ad5
Victor Aldair Quispe Baca <130853409+230305609@users.noreply.github.com>
2023-05-12 15:06:13 -0300

    Update put_item_function.js.js

--- src/put_item_function.js.js
+++ src/put_item_function.js.js
@@ -1,13 +1,39 @@
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-}
+var AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    
+    let responseBody = ""
+    let statusCode = 0
+    
+    let {id, price} = JSON.parse(event.body);
+    
+    const params = {
+      TableName : 'Items',
+      /* Item properties will depend on your application concerns */
+      Item: {
+         id: id,
+         price: price
+      }
+    }
+    
+    try {
+        
+        await dynamodb.put(params).promise();
+        statusCode = 200;
+        responseBody = JSON.stringify('Item inserido com sucesso!');
+        
+    } catch (err) {
+          
+        statusCode = 200;
+        responseBody = JSON.stringify(err);
+        
+    }
+      
+    const response = {
+        statusCode: statusCode,
+        body: responseBody,
+    };
+    
+    return response;
+};
